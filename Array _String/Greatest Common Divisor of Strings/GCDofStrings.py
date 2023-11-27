ea5a17ab648acc2e25bf2a9a441d55c9af8ac50b
aisfak07 <aisfak07@gmail.com>
2023-11-27 21:06:49 +0600

    two problem solved from LeetCode75 with python

--- Array _String/Greatest Common Divisor of Strings/GCDofStrings.py
+++ Array _String/Greatest Common Divisor of Strings/GCDofStrings.py
@@ -1,15 +1,22 @@
-class Solution:
+class Solution:    
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        newStr = ""
-        for i in range(min(len(str1), len(str2))):
-            if str1[i] == str2[i]:
-                if str1[i] not in newStr:
-                    newStr += str1[i]
+        # print(str1)
+        if len(str1) < len(str2):
+            return self.gcdOfStrings(str2, str1)
         
-        return newStr
+        elif not str1.startswith(str2):
+            return ""
+        
+        elif len(str1) == len(str2) :
+            return str1
+        
+        else:
+            return self.gcdOfStrings(str1[len(str2):], str2)
+        
 
 
-str1 = "LEET"
-str2 = "CODE"
+str1 = "ABC"
+str2 = "ABCDEF"
 z = Solution()
-print(z.gcdOfStrings(str1, str2))
+zyx = z.gcdOfStrings(str1, str2)
+print(zyx)
